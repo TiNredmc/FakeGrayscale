3f4713328581ebb54ba7bea0ede0d19d424542aa
TINLΞTHΛX <thipok17@hotmail.com>
2019-04-09 20:51:33 +0700

    removed the unnecessary things

--- FakeGrayscale.ino
+++ FakeGrayscale.ino
@@ -1,29 +1,13 @@
 /* THIS CODE IS DESIGNED FOR ESP-01S. Other MCU might works */
 
 #include <GU7000_Interface.h>
-//#include <GU7000_Parallel.h>
 #include <GU7000_Serial_Async.h>
-//#include <GU7000_Serial_Sync.h>
-//#include <GU7000_Serial_SPI.h>
 #include <Noritake_VFD_GU7000.h>
 #include "BMPs.h"
-// ****************************************************
-// ****************************************************
-// Uncomment one of the communication interfaces below.
-//
 
 GU7000_Serial_Async interface(115200,1, 2, 3); // BAUD RATE,SIN(pin TX),BUSY(pin GPOIO2),RESET(pin RX)
-//GU7000_Serial_Sync interface(3, 5, 6, 7); // SIN,BUSY,SCK,RESET
-//GU7000_Serial_SPI interface(3, 5, 6, 7, 8); // SIN,BUSY,SCK,RESET,CS
-//GU7000_Parallel interface('R', 8,9,10,11, 0,1,2,3,4,5,6,7); // Module Pin#3=RESET; BUSY,RESET,WR,RD,D0-D7
-//GU7000_Parallel interface('B', 8,9,10,11, 0,1,2,3,4,5,6,7); // Module Pin#3=BUSY; BUSY,RESET,WR,RD,D0-D7
-//GU7000_Parallel interface('N', 8,9,10,11, 0,1,2,3,4,5,6,7); // Module Pin#3=nothing; BUSY,RESET,WR,RD,D0-D7
-//
-// ****************************************************
-// ****************************************************
 
 Noritake_VFD_GU7000 vfd;
-
 
 void setup() {
   pinMode(1, FUNCTION_3);
