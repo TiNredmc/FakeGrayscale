66bd65ff883536b604fbda8243578937a8285097
TINLΞTHΛX <thipok17@hotmail.com>
2019-04-09 13:25:41 +0700

    Make the code AVR friendly.

--- FakeGrayscale.ino
+++ FakeGrayscale.ino
@@ -10,7 +10,7 @@
 // Uncomment one of the communication interfaces below.
 //
 
-GU7000_Serial_Async interface(115200,1, 2, 3); // BAUD RATE,SIN(pin TX),BUSY(pin GPOI2),RESET(pin RX)
+GU7000_Serial_Async interface(115200,2, 3, 4); // BAUD RATE,SIN(pin 2),BUSY(pin 3),RESET(pin 4)
 //GU7000_Serial_Sync interface(3, 5, 6, 7); // SIN,BUSY,SCK,RESET
 //GU7000_Serial_SPI interface(3, 5, 6, 7, 8); // SIN,BUSY,SCK,RESET,CS
 //GU7000_Parallel interface('R', 8,9,10,11, 0,1,2,3,4,5,6,7); // Module Pin#3=RESET; BUSY,RESET,WR,RD,D0-D7
@@ -26,8 +26,6 @@ Noritake_VFD_GU7000 vfd;
 
 
 void setup() {
-  pinMode(1, FUNCTION_3);
-  pinMode(3, FUNCTION_3);
   _delay_ms(700);           // wait for device to power up
   vfd.begin(280, 16);       // In this case. I use GU280X16G-7000 VFD module.
   vfd.interface(interface); // select which interface to use. In this case I use Async interface (It's actually RS232)
