3007eadf3847da49ad7558a116c668f2e9f748aa
TINLΞTHΛX <thipok17@hotmail.com>
2019-04-10 12:28:29 +0700

    added more comments

--- FakeGrayscale.ino
+++ FakeGrayscale.ino
@@ -1,23 +1,23 @@
 /* THIS CODE IS DESIGNED FOR ESP-01S. Other MCU might works */
 
-#include <GU7000_Interface.h>
-#include <GU7000_Serial_Async.h>
-#include <Noritake_VFD_GU7000.h>
-#include "BMPs.h"
+#include <GU7000_Interface.h> // include the Interface hub header
+#include <GU7000_Serial_Async.h> // include the Async comm header
+#include <Noritake_VFD_GU7000.h> // include the main header
+#include "BMPs.h" // include our bitmaps hex code (this will put in the MCU's EEPROM)
 
 GU7000_Serial_Async interface(115200,1, 2, 3); // BAUD RATE,SIN(pin TX),BUSY(pin GPOIO2),RESET(pin RX)
 
-Noritake_VFD_GU7000 vfd;
+Noritake_VFD_GU7000 vfd; 
 
 void setup() {
-  pinMode(1, FUNCTION_3);
-  pinMode(3, FUNCTION_3);
+  pinMode(1, FUNCTION_3); // set pinMode for the TX
+  pinMode(3, FUNCTION_3); // set pinMode for the RX
   _delay_ms(700);           // wait for device to power up
   vfd.begin(280, 16);       // In this case. I use GU280X16G-7000 VFD module.
   vfd.interface(interface); // select which interface to use. In this case I use Async interface (It's actually RS232)
-  vfd.isModelClass(7000);
+  vfd.isModelClass(7000); // My display is -7000. GU7000 series have a lots of 
   //vfd.isGeneration('B');    // Uncomment this for B generation
-  vfd.GU7000_reset();       // reset module
+  vfd.GU7000_reset();       // reset module. to clear everthing remain on the memory and display.
   vfd.GU7000_init();        // initialize module
 }
 
